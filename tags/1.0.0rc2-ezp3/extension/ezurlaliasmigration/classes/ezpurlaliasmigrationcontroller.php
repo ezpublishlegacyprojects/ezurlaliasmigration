0b1be000385a0e88ab9ce91f3e0c62fbf94ac298
oms <info@example.com>
2008-08-08 11:25:36 +0000

    - Added tags for RC2 releases included in eZP 4.0.1rc2 and 3.10.1rc2
    git-svn-id: http://svn.projects.ez.no/ezurlaliasmigration@31 e31bd701-45f7-4eca-988e-bcc9f253875d

---
+++ tags/1.0.0rc2-ezp3/extension/ezurlaliasmigration/classes/ezpurlaliasmigrationcontroller.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * File containing the ezpUrlAliasMigrationController class
+ *
+ * @copyright Copyright (C) 1999-2008 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
+ * @package UrlAliasMigration
+ *
+ */
+
+/**
+ * Super class for controllers used in the url alias migration extension.
+ * 
+ * This class simply provide shared functionality between the controllers, such as
+ * script progress output.
+ *
+ * @package UrlAliasMigration
+ */
+class ezpUrlAliasMigrationController
+{
+    /**
+     * Set callback function which can be used to get progess report from the controller
+     * classes when doing migration oeprations.
+     *
+     * @param callback $callback
+     * @static
+     * @return void
+     */
+    function setProgressCallback( $callback )
+    {
+        $callbackMethod =& $GLOBALS['ezpUrlAliasMigrationController-callbackMethod'];
+        $callbackMethod = $callback;
+    }
+
+    /**
+     * Set the number of iteration for the current operation to $count.
+     * 
+     * If no callback method have been defined for the class, e.g. the controller is
+     * triggered from web gui, no call will be made to eZScript.
+     * 
+     * @param int $count
+     * @static
+     * @return void
+     */
+    function setProgressCount( $count )
+    {
+        $callback =& $GLOBALS['ezpUrlAliasMigrationController-callbackMethod'];
+        if ( $callback !== null )
+        {
+            $script =& eZScript::instance();
+            $script->resetIteration( $count );
+        }
+    }
+
+    /**
+     * Triggers the callback function is one has been defined, if not, no action will
+     * be performed.
+     *
+     * @param boolean $result 
+     * @static
+     * @return void
+     */
+    function doCallback( $result )
+    {
+        $callback =& $GLOBALS['ezpUrlAliasMigrationController-callbackMethod'];
+        if ( $callback !== null )
+        {
+            call_user_func( $callbackMethod, $result );
+        }
+    }
+}
+?>
